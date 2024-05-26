cb9ea1f562dc4e46f071ef02abb4e213867dd1c0
Mateus <mateuscst45@gmail.com>
2024-05-26 16:55:15 -0300

    Final Adjustments made

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import "./App.scss";
+import image from "./assets/image2.png";
 
 const AboutUs = () => {
   return (
     <div className="homePage">
-      <div className="textBox"></div>
+      <div className="imageBox">
+        <img src={image} alt="" />
+      </div>
       <div className="textBox">
         <h2 className="title">This store is not real!</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
           quam pellentesque nec nam aliquam sem. Tellus molestie nunc non
-          blandit massa enim nec dui nunc.
+          blandit massa enim nec dui nunc. This image was made by Freepik.
         </p>
       </div>
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./App.scss";
+import image from "./assets/image.png";
 
 const Home = () => {
   return (
@@ -12,11 +13,14 @@ const Home = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
           quam pellentesque nec nam aliquam sem. Tellus molestie nunc non
-          blandit massa enim nec dui nunc.
+          blandit massa enim nec dui nunc. This image was made by Freepik.
         </p>
         <Link to={"/Shop"}>
           <button>Meet our courses</button>
         </Link>
+      </div>
+      <div className="imageBox">
+        <img src={image} alt="" />
       </div>
     </div>
   );
